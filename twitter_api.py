d2a8c0a8239aaa67f7d4f340ee2351bb557c0b73
RameezAnwar <ranwar25@gmail.com>
2022-09-27 20:15:51 -0500

    api connection setup and working

--- twitter_api.py
+++ twitter_api.py
@@ -12,7 +12,19 @@ api_key = config['twitter']['api_key']
 api_key_secret = config['twitter']['api_key_secret']
 
 access_token = config['twitter']['access_token']
-access_token = config['twitter']['access_token_secret']
+access_token_secret = config['twitter']['access_token_secret']
 
 
-print(api_key)
+#print(api_key)
+
+#get aunthentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
