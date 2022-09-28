a43ed1c7d3137c177417b027830b0e428137b2b4
RameezAnwar <ranwar25@gmail.com>
2022-09-27 20:29:21 -0500

    panda dataframe, saving data to file, comments for explanation

--- twitter_api.py
+++ twitter_api.py
@@ -1,5 +1,6 @@
 import tweepy
 import configparser
+import pandas as pd
 
 
 #get credentials
@@ -14,7 +15,7 @@ api_key_secret = config['twitter']['api_key_secret']
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
-
+#check if config file working
 #print(api_key)
 
 #get aunthentication
@@ -26,5 +27,21 @@ api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
 
+#test if api connection is working
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+columns = ['Time', 'User', 'Tweet']
+data = []
 for tweet in public_tweets:
-    print(tweet.text)
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
+
+#print(data)
+
+#adding tweet to dataframe to be saveable
+df = pd.DataFrame(data, columns = columns)
+#print(df)
+
+df.to_csv( 'tweets.csv')
+
+
