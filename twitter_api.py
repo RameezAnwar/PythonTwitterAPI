1e26a04db6902bd88e4b8abcc152559a5d204d0e
RameezAnwar <ranwar25@gmail.com>
2022-09-28 20:23:46 -0500

    getting user tweet functionality

--- twitter_api.py
+++ twitter_api.py
@@ -25,12 +25,16 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-public_tweets = api.home_timeline()
+
 
 #test if api connection is working
+public_tweets = api.home_timeline()
 #for tweet in public_tweets:
 #    print(tweet.text)
 
+
+
+#checking panda functionality
 columns = ['Time', 'User', 'Tweet']
 data = []
 for tweet in public_tweets:
@@ -38,10 +42,44 @@ for tweet in public_tweets:
 
 #print(data)
 
-#adding tweet to dataframe to be saveable
+#adding tweets to dataframe to be saveable
 df = pd.DataFrame(data, columns = columns)
 #print(df)
 
+#saving tweets to csv file
 df.to_csv( 'tweets.csv')
+
+
+###########################################################################################
+
+#getting tweets from specific user
+user = 'veritasium'
+limit = 300
+
+#tweepy method of bypassing twitter 200 limit
+tweets = tweepy.Cursor(api.user_timeline,  screen_name = user, 
+count = 200, tweet_mode = 'extended').items(limit)
+
+#without using limit bypass
+#tweets = api.user_timeline(screen_name = user,count = limit, tweet_mode = 'extended' )
+
+#for tweet in tweets:
+#    print( tweet.full_text)
+
+
+#saving user tweets to dataframe and saving it
+user_columns = ['User', 'Tweet']
+user_data = []
+
+for tweet in tweets:
+    user_data.append([tweet.user.screen_name, tweet.full_text])
+
+user_df = pd.DataFrame(user_data, columns = user_columns)
+
+#print(user_df)
+user_df.to_csv( 'user_tweets.csv')
+
+
+#####################################################################################
 
 
