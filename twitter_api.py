ed5df7499573c4b18f970b068a52f69d2686cfa7
RameezAnwar <ranwar25@gmail.com>
2022-10-03 19:42:44 -0500

    word count analysis added

--- twitter_api.py
+++ twitter_api.py
@@ -1,6 +1,28 @@
 import tweepy
 import configparser
 import pandas as pd
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import seaborn as sns
+
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk import sent_tokenize, word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+import pandas as pd
+import numpy as np
+import re
+import datetime as dt
+import spacy
+#only run line 19 the first time if a import error occurs for line 20
+#spacy.cli.download('en_core_web_lg')
+nlp = spacy.load('en_core_web_lg')
+
+
+
 
 
 #get credentials
@@ -47,7 +69,7 @@ df = pd.DataFrame(data, columns = columns)
 #print(df)
 
 #saving tweets to csv file
-df.to_csv( 'tweets.csv')
+#df.to_csv( 'tweets.csv')
 
 
 ###########################################################################################
@@ -77,19 +99,20 @@ for tweet in user_tweets:
 user_df = pd.DataFrame(user_data, columns = user_columns)
 
 #print(user_df)
-user_df.to_csv( 'user_tweets.csv')
+#user_df.to_csv( 'user_tweets.csv')
 
 
 #####################################################################################
 
 
-#getting tweets off keyword
+#getting tweets off keywords
+#can be @username, a word or phrase, or a #hashtag
 searching = 'God of War'
-limit = 300
+limit = 10
 
-#to bypass set limit above 100
+#to bypass set limit above 100 and count to 100
 keyword_tweets = tweepy.Cursor(api.search_tweets, q = searching, 
-count = 100, tweet_mode = 'extended').items(limit)
+count = 10, tweet_mode = 'extended').items(limit)
 
 #without bypass
 #keyword_tweets = api.search_tweets(q = searching, count = limit, tweet_mode = 'extended' )
@@ -106,4 +129,151 @@ for tweet in keyword_tweets:
 keyword_df = pd.DataFrame(keyword_data, columns = keyword_columns)
 
 #print(keyword_df)
-keyword_df.to_csv( 'keyword_tweets.csv')
+#keyword_df.to_csv( 'keyword_tweets.csv')
+
+
+#########################################################################################################3
+
+
+#obtaining more data and simple analysis
+
+user = 'BarackObama'
+limit = 100
+
+tweets = []
+likes = []
+time = []
+
+user_tweets2 = tweepy.Cursor(api.user_timeline,  screen_name = user, 
+count = 200, tweet_mode = 'extended').items(limit)
+
+
+#obtaining tweets without retweets
+for tweet in user_tweets2:
+    if( tweet.retweeted == False and 'RT @' not in tweet.full_text): 
+        tweets.append( tweet.full_text)
+        likes.append( tweet.favorite_count)
+        time.append( tweet.created_at)
+
+user_df2 = pd.DataFrame( {'tweets':tweets, 'likes':likes, 'time':time} )
+
+#print(user_df2)
+
+#getting most liked tweets
+mostLiked = user_df2.loc[user_df2.likes.nlargest(10).index]
+#print(mostLiked)
+
+
+
+#make sure matplotlib working right
+#plt.plot([1,2,3,4], [1,4,9,16])
+#plt.ylabel('y numbers')
+#plt.xlabel('x numbers')
+#plt.show()
+
+
+###############################################################################################
+
+#more in depth semantic/word analysis with spacy using user tweets
+
+#tweets between these dates
+begin_data = (2022, 8, 1)
+end_date = (2022, 8, 15)
+
+limit = 300
+lang = 'english'
+text = []
+
+analysis_user = 'BarackObama'
+analysis_tweets = tweepy.Cursor(api.user_timeline,  screen_name = analysis_user, 
+count = 100, tweet_mode = 'extended').items(limit)
+
+#getting rid of retweets
+for tweet in analysis_tweets:
+    if( tweet.retweeted == False and 'RT @' not in tweet.full_text): 
+        text.append( tweet.full_text)
+
+analysis_df = pd.DataFrame( {'text': text })
+#getting only the text
+analysis_df = analysis_df['text']
+#print(analysis_df)
+
+
+sentences = []
+
+for word in analysis_df:
+    sentences.append(word)
+
+#print('SENTENCES')
+#print(sentences)
+
+
+#splitting list into single words
+lines = list()
+for line in sentences:
+    words = line.split()
+    for w in words:
+        lines.append(w)
+
+#print('LINES')
+#print(lines)
+
+
+#removing punctuation
+lines = [re.sub( r'[^A-Za-z0-9]+', '', x) for x in lines]
+
+lines2 = []
+for words in lines:
+    if words != '':
+        lines2.append(words)
+
+#print('LINES2')
+#print(lines2)
+
+
+#using stem to make words into root version
+s_stemmer = SnowballStemmer( language = 'english')
+
+stem = []
+for word in lines2:
+    stem.append( s_stemmer.stem(word))
+
+#print('STEM')
+#print(stem)
+
+#remove stop words 
+stem2 = []
+for word in stem:
+    if word not in nlp.Defaults.stop_words:
+        stem2.append(word)
+
+#print('STEM2')
+#print(stem2)
+
+
+#now that we have the data for analysis time to store it in 
+#a new dataframe to use for visualizations
+analysis_df2 = pd.DataFrame(stem2)
+#group words on count
+analysis_df2 = analysis_df2[0].value_counts()
+
+#print( analysis_df2)
+
+#getting frequencies of words
+freqdetect = FreqDist()
+
+for words in analysis_df:
+    freqdetect[words] += 1
+
+#word count analysis 
+#doing visualizations
+analysis_df2 = analysis_df2[:30,]
+plt.figure( figsize = (10,5))
+sns.barplot( x = analysis_df2.values, y = analysis_df2.index, alpha = 0.8)
+
+plt.title('Top Words Used')
+plt.ylabel('Word From Tweet', fontsize = 12)
+plt.xlabel('Count Of Words', fontsize = 12)
+plt.show()
+
+
