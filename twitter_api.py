42397bb216b366525cb4de80c21109055b54aaae
RameezAnwar <ranwar25@gmail.com>
2022-10-03 20:45:07 -0500

    org and people analysis and visualizations added

--- twitter_api.py
+++ twitter_api.py
@@ -17,8 +17,13 @@ import numpy as np
 import re
 import datetime as dt
 import spacy
-#only run line 19 the first time if a import error occurs for line 20
+from spacy import displacy
+from collections import Counter
+#only the following spacy.cli.download lines the first time 
+#running the code if an import error 
+#occurs for the spacy.load line
 #spacy.cli.download('en_core_web_lg')
+#spacy.cli.download('en_core_web_sm')
 nlp = spacy.load('en_core_web_lg')
 
 
@@ -274,6 +279,65 @@ sns.barplot( x = analysis_df2.values, y = analysis_df2.index, alpha = 0.8)
 plt.title('Top Words Used')
 plt.ylabel('Word From Tweet', fontsize = 12)
 plt.xlabel('Count Of Words', fontsize = 12)
+#plt.show()
+
+
+#hange spacy load
+nlp = spacy.load('en_core_web_sm')
+
+
+def show_ents(doc):
+    if doc.ents:
+        for ent in doc.ents:
+            print(ent.text + ' - ' + ent.label_ + ' - ' + str(spacy.explain(ent.label_)))
+
+
+#getting organizations from tweets
+str1 = " "
+stem2 = str1.join(lines2)
+
+stem2 = nlp(stem2)
+
+label = [ (X.text, X.label_) for X in stem2.ents]
+
+analysis_df3 = pd.DataFrame( label, columns = ['Word', 'Entity'])
+
+org_df = analysis_df3.where( analysis_df3['Entity'] == 'ORG')
+org_df = org_df['Word'].value_counts()
+
+
+#org visualization
+org_df = org_df[:20,]
+plt.figure( figsize=(10,5))
+sns.barplot( x = org_df.values, y = org_df.index, alpha = 0.8)
+plt.title('Top Organizations Mentioned')
+plt.ylabel('Word From Tweet', fontsize = 12)
+plt.xlabel('Count Of Words', fontsize = 12)
+plt.tight_layout()
+#plt.show()
+
+
+
+#getting people mentioned 
+str2 = " "
+stem2 = str2.join(lines2)
+
+stem2 = nlp(stem2)
+
+label2 = [ (X.text, X.label_) for X in stem2.ents]
+
+analysis_df4 = pd.DataFrame( label2, columns = ['Word', 'Entity'])
+
+person_df = analysis_df4.where( analysis_df4['Entity'] == 'PERSON')
+person_df = person_df['Word'].value_counts()
+
+
+#people visualization
+person_df = person_df[:20,]
+plt.figure( figsize=(10,5))
+sns.barplot( x = person_df.values, y = person_df.index, alpha = 0.8)
+plt.title('Top People Mentioned')
+plt.ylabel('Word From Tweet', fontsize = 12)
+plt.xlabel('Count Of Words', fontsize = 12)
+plt.tight_layout()
 plt.show()
-
-
